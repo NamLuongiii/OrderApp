0a1b3485378ed5ec101ac11883a1670e0199ed56
namluong <luongkhacnam222@gmail.com>
2026-05-16 15:27:01 +0700

    update

--- app/common/postgresql/Connection.go
+++ app/common/postgresql/Connection.go
@@ -2,13 +2,23 @@ package postgresql
 
 import (
 	"OrderApp/persistency/table"
+	"fmt"
+	"os"
 
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func NewConnection() (*gorm.DB, error) {
-	dsn := "host=localhost user=postgres password=postgres dbname=orderapp port=5432 sslmode=disable"
+	// 1. Đọc DB_HOST từ môi trường, nếu trống thì mặc định là localhost (cho máy Mac)
+	dbHost := os.Getenv("DB_HOST")
+	if dbHost == "" {
+		dbHost = "localhost"
+	}
+
+	// 2. Tự động ráp vào chuỗi DSN bằng fmt.Sprintf
+	dsn := fmt.Sprintf("host=%s user=postgres password=postgres dbname=orderapp port=5432 sslmode=disable", dbHost)
+
 	db, e := gorm.Open(postgres.Open(dsn))
 	if e != nil {
 		return nil, e
