3f720ceaad1ac4132a7c1e51ccd08abd25312196
namluong <luongkhacnam222@gmail.com>
2026-05-16 15:08:16 +0700

    update

--- app/common/cors/Index.go
+++ app/common/cors/Index.go
@@ -4,11 +4,19 @@ import "github.com/gin-gonic/gin"
 
 func MiddlewareCors() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "*") // Cho phép tất cả các nguồn
+		// Lấy domain của thằng gọi API
+		origin := c.Request.Header.Get("Origin")
+
+		// Nếu có origin (trình duyệt gửi lên), set thẳng origin đó thay vì dùng "*"
+		if origin != "" {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS, GET, PUT, DELETE")
 
+		// Xử lý Preflight request (OPTIONS)
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
 			return
